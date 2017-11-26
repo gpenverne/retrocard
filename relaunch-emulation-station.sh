d5149951e2f2667e7f8a0e3576c9c5096eb4bc19
gpenverne <gpenverne@gmail.com>
2017-11-26 11:21:51 +0100

    Relaunch emulastation on unplug

--- relaunch-emulation-station.sh
+++ relaunch-emulation-station.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-USBPATH="/media/RETROCARD"
+USBPATH="/media/retrocard"
 RETROCARD_PATH="$USBPATH/.retrocard"
 if [ ! -d "$RETROCARD_PATH" ]; then
     exit
 fi
 
+/home/pi/retrocard/kill-emulator.sh
 /usr/bin/emulationstation
