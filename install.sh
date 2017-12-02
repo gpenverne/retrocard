500eede3f178e34ffe9d9edbb25e1be8968568e9
Grégoire <gpenverne@users.noreply.github.com>
2017-12-02 09:56:46 +0100

    Use cdrom as retrocard (#5)
    * Use cdrom as retrocard
    
    * fix path

--- install.sh
+++ install.sh
@@ -19,3 +19,7 @@ systemctl enable retrocard.umount.service
 chmod +x /home/pi/retrocard/launch.sh
 chmod +x /home/pi/retrocard/kill-emulator.sh
 chmod +x /home/pi/retrocard/relaunch-emulation-station.sh
+
+cp /home/pi/retrocard/99-disc_script.rules /etc/udev/rules.d/99-disc_script.rules
+chmod +x /home/pi/retrocard/mount-cdrom-as-retrocard.sh
+udevadm control --reload
