8c888a7c9c3fde17847d2a3bcbcdac0e0c4a20d8
gpenverne <gpenverne@gmail.com>
2017-12-03 12:05:05 +0100

    Take system platform and rom path from arguments if provided

--- launch.sh
+++ launch.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
-#!/
+
 /usr/bin/cec-client &
 /home/pi/retrocard/kill-emulator.sh
 
 echo "LAUNCHING" > /tmp/retrocard.log
 USBPATH="/media/retrocard"
 RETROCARD_PATH="$USBPATH/.retrocard"
-if [ ! -d "$RETROCARD_PATH" ]; then
-    exit
+if [ $# -eq 0 ]; then
+    if [ ! -d "$RETROCARD_PATH" ]; then
+        exit
+    fi
+    SYSTEM_PLATFORM=`cat $RETROCARD_PATH/SYSTEM`
+    ROM_FILE=`cat $RETROCARD_PATH/ROM`
+    ROM_PATH="$USBPATH/$ROM_FILE"
+else
+    SYSTEM_PLATFORM=$1
+    ROM_PATH=$2
+    USBPATH=""
 fi
 
 RETROPIE_RUNCOMMAND_PATH="sudo /opt/retropie/supplementary/runcommand/runcommand.sh 1 _SYS_"
-SYSTEM_PLATFORM=`cat $RETROCARD_PATH/SYSTEM`
-ROM_FILE=`cat $RETROCARD_PATH/ROM`
-ROM_PATH="$USBPATH/$ROM_FILE"
-
 LAUNCH_COMMAND="$RETROPIE_RUNCOMMAND_PATH $SYSTEM_PLATFORM $ROM_PATH $USBPATH"
 echo $LAUNCH_COMMAND >> /tmp/retrocard.log
 $LAUNCH_COMMAND > /dev/null
@@ -23,5 +28,6 @@ RUNCOMMAND=`tail -n 1 $RUNCOMMANDLOG_PATH`
 RUNCOMMAND=${RUNCOMMAND:11}
 killall emulationstation
 echo $RUNCOMMAND >> /tmp/retrocard.log
-eval $RUNCOMMAND > /dev/null
+eval $RUNCOMMAND > /dev/null &
+
 emulationstation
